405b151da9460fc932487a0c6c101f1bfc391dc8
Raaba <raaba21@gmail.com>
2023-11-09 14:30:58 -0500

    added current weather report functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,19 @@ $(".searchBtn").on("click", function(){
         url: `http://api.openweathermap.org/geo/1.0/direct?q=${searchEl}&limit=1&appid=27003bda8c7c57abc371f9200fd76b09`,
         success: function(results){
             $.ajax({
-                url: `http://api.openweathermap.org/data/2.5/forecast?lat=${results[0].lat}&lon=${results[0].lon}&appid=27003bda8c7c57abc371f9200fd76b09`,
+                url: `https://api.openweathermap.org/data/2.5/weather?lat=${results[0].lat}&lon=${results[0].lon}&appid=27003bda8c7c57abc371f9200fd76b09&units=metric`,
                 success: function(results){
                     console.log(results)
                 }
+            }).done(function(){
+                $.ajax({
+                    url: `http://api.openweathermap.org/data/2.5/forecast?lat=${results[0].lat}&lon=${results[0].lon}&appid=27003bda8c7c57abc371f9200fd76b09&units=metric`,
+                    success: function(results){
+                        console.log(results)
+                    }
+                })
             })
+            
         }
 
     })
