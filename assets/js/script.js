3fd6dff2c8b118db72ba99f9be31f6e6a85c63e7
Raaba <raaba21@gmail.com>
2023-11-09 13:36:37 -0500

    added ajax api calling and search bar input functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
 var today = dayjs().format("YYYY")
 
 $(".searchBtn").on("click", function(){
-    $(".citynTime").text($("#searchCity").val())
-    
+    var searchEl = $("#searchCity").val()
+    $(".citynTime").text(searchEl+" "+today)
+    $.ajax({
+        url: `http://api.openweathermap.org/geo/1.0/direct?q=${searchEl}&limit=1&appid=27003bda8c7c57abc371f9200fd76b09`,
+        success: function(results){
+            $.ajax({
+                url: `http://api.openweathermap.org/data/2.5/forecast?lat=${results[0].lat}&lon=${results[0].lon}&appid=27003bda8c7c57abc371f9200fd76b09`,
+                success: function(results){
+                    console.log(results)
+                }
+            })
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
 
 })
 
