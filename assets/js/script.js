24a5a60725fa595f1804de7e2482df0ab45df629
Raaba <raaba21@gmail.com>
2023-11-11 12:14:21 -0500

    added a feature that displays an icon corresponding to the weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,10 @@ $(".searchBtn").on("click", function () {
         url: `https://api.openweathermap.org/data/2.5/weather?lat=${results[0].lat}&lon=${results[0].lon}&appid=27003bda8c7c57abc371f9200fd76b09&units=metric`,
         success: function (results) {
             $("#citynTime")[0].innerText = searchEl + " " + dayjs().format("MM-DD-YYYY")
-            $("#cityTemp")[0].innerText = "Temp: " + results.main.temp + "C"
+            $("#cityTemp")[0].innerText = "Temp: " + results.weather.temp + "C"
+            console.log(results)
+            var iconURL = "http://openweathermap.org/img/w/" + results.weather[0].icon + ".png"
+            $("#icon")[0].setAttribute("src", iconURL)
         },
         // Getting current weather info from the city 
       }).done(function () {
